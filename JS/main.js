280db3661ac9d9add7bd6b87708ae4c253bb3c61
YanissCedriik <167817702+YanissCedriik@users.noreply.github.com>
2024-08-20 16:04:38 +0200

    QOF changes

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,38 @@
 const app = document.getElementById("app");
-const roundTime = document.getElementById("lastRound");
-let milliseconds,
+const roundTime = document.getElementById("lastRound");   
+let milliseconds = 0,
   seconds = 0,
-  minuets = 0,
-  hours = 0;
+  minuets = 0,                              
+  hours = 0;              
+  days = 0;
 var timer;
 
-function startTimer() {
-  setInterval(countSeconds, 10);
+function startTimer() {                 
+  setInterval(countSeconds, 10);                // Starter timer, med intervaller på 10 millisekunder
 }
 
 function countSeconds() {
-  app.innerHTML = `${hours}:${minuets}:${seconds}:${milliseconds}`;
-  milliseconds++;
+  app.innerHTML = `${days}:${hours}:${minuets}:${seconds}:${milliseconds}`;       // Endrer hva som blir vist i  <div id= "app">
+  milliseconds++;           
 
-  if (milliseconds == 100) {
+  if (milliseconds == 100) {                //Hvis millisekunder blir 100 --> legg til 1 sekund og restart millisekunder
     seconds++;
+    milliseconds = 00;
   }
 
-  if (seconds == 60) {
+  if (seconds == 60) {                      //Hvis sekunder blir 60 --> legg til 1 minutt og restart sekunder
     minuets++;
+    seconds = 0;
+  }
+
+  if (minuets == 60 ){                      //Hvis minutter blir 60 --> legg til 1 time og restart minutter
+    hours++;
+    minuets = 0;
+  }
+
+  if (hours == 24){                         //Hvis du ikke ser mønsteret --> Logg av
+    days++;
+    hours = 0;
   }
 
 
@@ -27,11 +40,11 @@ function countSeconds() {
 }
 
 function pauseTimer() {
-  roundTime.innerHTML += `<li>${hours}:${minuets}:${seconds}:${milliseconds}</li>`;
+  roundTime.innerHTML += `<li>${days}:${hours}:${minuets}:${seconds}:${milliseconds}</li>`;       //Trykk på knappen for å vise tid i roundTime (Output definert på linje 2)
   clearInterval(timer);
   milliseconds = 0;
 }
 
-function stopAll() {
+function stopAll() {                      // Reload siden
   location.reload();
 }
